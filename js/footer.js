af4d3daab3f764802c423ac54bb4639f0a6ad612
Guillaume-BL <kawiess@gmail.com>
2025-04-03 11:31:17 +1100

    First commit (après le fork)

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,5 @@
 class Footer extends HTMLElement {
-    constructor() {
-        super();
-    }
-
+    constructor() { super(); }
     connectedCallback() {
         this.innerHTML = `
             <footer class="footer py-5">
@@ -11,11 +8,10 @@ class Footer extends HTMLElement {
                         <!-- Colonne 1 : Logo et description -->
                         <div class="col-md-3 mb-4">
                             <img src="../../public/logo-gite-BLANC.png" alt="Logo Gîte Pim" width="200" class="mb-3">
-                             <h6 class="text-uppercase fw-bold text-white mb-3">Contact</h6>
+                            <h6 class="text-uppercase fw-bold text-white mb-3">Contact</h6>
                             <p class="text-white"><i class="bi bi-geo-alt-fill me-2"></i> Îlot Pam, Poum</p>
                             <p class="text-white"><i class="bi bi-telephone-fill me-2"></i> +687 12 34 56</p>
                             <p class="text-white"><i class="bi bi-envelope-fill me-2"></i> contact@gitepim.nc</p>
-                            
                         </div>
                         <!-- Colonne 2 : Navigation principale -->
                         <div class="col-md-3 mb-4">
@@ -24,11 +20,8 @@ class Footer extends HTMLElement {
                                 <li><a href="../../index.html" class="text-white text-decoration-none">Accueil</a></li>
                                 <li><a href="../chambre/chambre-index.html" class="text-white text-decoration-none">Nos chambres</a></li>
                                 <li><a href="../repas/repas-index.html" class="text-white text-decoration-none">Notre restaurant</a></li>
-                                
                                 <li><a href="./modules/garderie/garderie-index.html" class="text-white text-decoration-none">La garderie</a></li></br></br>
                             </ul>
-                       
-                      
                             <h6 class="text-uppercase fw-bold text-white mb-3">Activité</h6>
                             <ul class="list-unstyled">
                                 <li><a href="../cheval/cheval-index.html" class="text-white text-decoration-none">Randonnée équestre</a></li>
@@ -39,7 +32,7 @@ class Footer extends HTMLElement {
                         <!-- Colonne 3 : Contact et réseaux sociaux -->
                         <div class="col-md-3 mb-4">
                         <img src="../../public/cartepim.png" alt="lieux Gîte Pim" width="300" class="mb-3">
-                           <h6 class="text-uppercase fw-bold text-white mt-4">Suivez-nous</h6>
+                            <h6 class="text-uppercase fw-bold text-white mt-4">Suivez-nous</h6>
                             <div>
                                 <a href="#" class="text-white me-3"><img src="../../public/facebook.png" alt="Facebook" width="24"></a>
                                 <a href="#" class="text-white me-3"><img src="../../public/instagram.png" alt="Instagram" width="24"></a>
@@ -53,4 +46,5 @@ class Footer extends HTMLElement {
     }
 }
 
+// Enregistrement de la balise personnalisée.
 customElements.define('pied-de-page', Footer);
--- js/navbar.js
+++ js/navbar.js
@@ -1,45 +1,43 @@
 class NavBar extends HTMLElement {
-	constructor() {
-		super();
-	}
+	constructor() { super(); }
 	connectedCallback() {
 		this.innerHTML = `
 			<nav class="navbar navbar-dark navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="../../index.html">
-      <img src="../../public/logo-gite-BLANC.png" alt="Accueil" width="160" height="60" class="d-inline-block align-text-top" title="Accueil">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-      <ul class="navbar-nav fs-5">
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../chambre/chambre-index.html">Nos chambres</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../repas/repas-index.html">Notre restaurant</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../cheval/cheval-index.html">Randonnée équestre</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../kayak/kayak-index.html">Sortie en kayak</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../bagne/bagne-index.html">Visite du bagne</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../garderie/garderie-index.html">La garderie</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+				<div class="container-fluid">
+					<a class="navbar-brand" href="../../index.html">
+						<img src="../../public/logo-gite-BLANC.png" alt="Accueil" width="160" height="60" class="d-inline-block align-text-top" title="Accueil">
+					</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+						aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+						<ul class="navbar-nav fs-5">
+							<li class="nav-item">
+								<a class="nav-link text-white" href="../chambre/chambre-index.html">Nos chambres</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="../repas/repas-index.html">Notre restaurant</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="../cheval/cheval-index.html">Randonnée équestre</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="../kayak/kayak-index.html">Sortie en kayak</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="../bagne/bagne-index.html">Visite du bagne</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="../garderie/garderie-index.html">La garderie</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 		`;
 	}
 }
 
-// Enregistrement de la balise personnalisée
+// Enregistrement de la balise personnalisée.
 customElements.define('menu-top', NavBar);
